10c4adf93eb605f70f5fe6106fb16a6c5fad4c22
Reggie Peterson <reginald.peterson2@nih.gov>
2019-04-15 18:02:52 -0400

    build mobile first, add favicon, add info and links

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
 $("#arrow").on("click",function(){
-    $(this).toggleClass("arrowTwo");
+    reset()
+    $(this).toggleClass("arrowTwo")
     $("#nav-ul").fadeToggle(600);
+    $('body').css('background-color','#ccc')
+
 
 })
+
+function reset() {
+    $('#about-button').css('color','#000')
+    $('#work-button').css('color','#000')
+    $('#arrow').css('color','#000')
+    $(".about-info").hide()
+    $(".work-info").hide()
+    $(".contact-info").hide()
+}
+
+$("#about-button").on("click", function(){
+    reset()
+    $(".about-info").fadeToggle()
+    $('body').css('background-color','#ccc')
+})
+
+$("#work-button").on("click", function(){
+    reset()
+    $(".work-info").fadeToggle()
+    $('body').css('background-color','#ccc')
+})
+
+$(".contact").on("click", function(){
+    reset()
+    $(".contact-info").fadeToggle()
+    $('body').css('background-color','#6d6c6c')
+    $('#about-button').css('color','#ccc')
+    $('#work-button').css('color','#ccc')
+    $('#arrow').css('color','#ccc')
+
+})
+
+
